dcf70b49db5c448d338336bd23d7cd3aa4f9cf20
Viscount <maple01@126.com>
2014-12-16 01:03:28 +0800

    Add content store

--- src/ccn/entity/ContentStore.java
+++ src/ccn/entity/ContentStore.java
@@ -1,5 +1,6 @@
 package ccn.entity;
 
+import java.util.Iterator;
 import java.util.List;
 
 public class ContentStore {
@@ -9,6 +10,32 @@ public class ContentStore {
 
 	public ContentStore() {
 		// TODO Auto-generated constructor stub
+	}
+	
+	public String get(int index){
+		return (String)detail.get(index);
+	}
+	
+	public int contains(String name){
+		int count = 0;
+		for (Iterator it = detail.iterator(); it.hasNext();){
+			String contentName = (String) it.next();
+			if (contentName.equals(name) ) return (count);
+			count++;
+		}
+		return -1 ;
+	}
+	
+	public int size(){
+		return detail.size();
+	}
+	
+	public void setCacheSize(int size){
+		cache_size = size;
+	}
+	
+	public int getCacheSize(){
+		return cache_size;
 	}
 
 }
