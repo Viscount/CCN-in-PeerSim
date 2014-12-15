1f6267990c09d486147eaa64804c99edfdce3f5d
Viscount <maple01@126.com>
2014-12-15 20:24:07 +0800

    First commit

---
+++ src/ccn/protocol/Infrastructure.java
@@ -0,0 +1,20 @@
+package ccn.protocol;
+
+import peersim.core.Node;
+import peersim.edsim.EDProtocol;
+import peersim.vector.SingleValueHolder;
+
+public class Infrastructure extends SingleValueHolder implements EDProtocol{
+
+	public Infrastructure(String prefix) {
+		// TODO Auto-generated constructor stub
+		super(prefix);
+	}
+
+	@Override
+	public void processEvent(Node arg0, int arg1, Object arg2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
